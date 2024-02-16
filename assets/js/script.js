fd6840b8178f15446e07347952c9c38221da21d1
nikhithareddy45 <nikkireddyperugu@gmail.com>
2024-02-16 09:56:18 +0530

    achi-block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,5 @@
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll(".nav-tag")
-window.onscroll=()=>{
-  sections.forEach(sec=>{
-    let top = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id')
-    if(top>=offset && top < offset + height){
-      navLinks.forEach(links=>{
-        links.classList.remove("active");
-        document.querySelector("nav a[href+=' + id + '] ").classList.add(".active");
-      })
-    }
-  })
-}
 //Projects
 const container = document.querySelectorAll(".project-img");
 const show=document.querySelector(".proj-desp")
